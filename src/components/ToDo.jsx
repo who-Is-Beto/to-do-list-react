5897a500f0b292110d8b585ffc0dfc09d3b42a55
Beto Cortés <roberto.cortes13233@gmail.com>
2021-01-17 17:38:31 -0600

    completed toggle done

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -8,11 +8,23 @@ const ToDo = ({ item, todos, setTodos }) => {
     setTodos(todos.filter((el) => el.id !== item.id))
   }
 
+  const handleCompleted = () => {
+    setTodos(todos.map((element) => {
+      if (element.id === item.id) {
+        return {
+          ...item,
+          completed: !item.completed
+        }
+      }
+      return item
+    }))
+  }
+
   return (
     <li>
       <h2>{item.text}</h2>
       <div className="item__options">
-        <small>{item.completed ? 'completed' : 'uncompleted'}</small>
+        <button type='button' onClick={handleCompleted}>{item.completed ? 'completed' : 'uncomplted'}</button>
         <button type='button' onClick={handleDeleteToDo}>Delete</button>
       </div>
     </li>
